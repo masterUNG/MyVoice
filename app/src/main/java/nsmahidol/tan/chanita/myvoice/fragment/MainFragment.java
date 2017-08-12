c195b9250ea8c80ce0945ede109e7f6e47934eb2
chanita <chanita_ou@yahoo.com>
2017-08-12 13:31:35 +0700

    change label ok

---
+++ app/src/main/java/nsmahidol/tan/chanita/myvoice/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package nsmahidol.tan.chanita.myvoice.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nsmahidol.tan.chanita.myvoice.R;
+
+/**
+ * Created by ADMIN on 12/8/2560.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return  view;
+    }
+} // Main Class
