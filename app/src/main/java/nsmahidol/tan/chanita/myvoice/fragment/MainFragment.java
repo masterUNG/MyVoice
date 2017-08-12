b175bfc30c4c1938bb04207346f1b305f2af43f9
chanita <chanita_ou@yahoo.com>
2017-08-12 15:06:50 +0700

    alert ok

--- app/src/main/java/nsmahidol/tan/chanita/myvoice/fragment/MainFragment.java
+++ app/src/main/java/nsmahidol/tan/chanita/myvoice/fragment/MainFragment.java
@@ -1,11 +1,15 @@
 package nsmahidol.tan.chanita.myvoice.fragment;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import nsmahidol.tan.chanita.myvoice.R;
 
@@ -20,5 +24,50 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         return  view;
+    } // omCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Icon1 Controller
+        icon1Controller();
+
+
+    } // onActivityCreate
+
+    private void icon1Controller() {
+        ImageView imageView = getView().findViewById(R.id.imvIcon1);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Show Dialog
+                CharSequence[] charSequences = new CharSequence[]{"เพศชาย", "เพศหญิง"};
+                final int[] intGender = {0};
+
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setCancelable(false);
+                builder.setIcon(R.mipmap.ic_alert);
+                builder.setTitle(R.string.chose_gender);
+                builder.setSingleChoiceItems(charSequences, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        intGender[0] = i;
+
+                        getActivity().getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.myContent, new ServiceFragment())
+                                .addToBackStack(null)
+                                .commit();
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
+
+
+
+            } //onClick
+        });
     }
 } // Main Class
