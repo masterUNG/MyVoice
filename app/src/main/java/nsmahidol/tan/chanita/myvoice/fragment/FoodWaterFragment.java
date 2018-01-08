51f603c4b5ad8e4d5d386c91f6debec619e546da
MasterUNG <phrombutr@gmail.com>
2018-01-08 15:44:25 +0700

    MP3 Success

--- app/src/main/java/nsmahidol/tan/chanita/myvoice/fragment/FoodWaterFragment.java
+++ app/src/main/java/nsmahidol/tan/chanita/myvoice/fragment/FoodWaterFragment.java
@@ -2,6 +2,7 @@ package nsmahidol.tan.chanita.myvoice.fragment;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -42,11 +43,16 @@ public class FoodWaterFragment extends Fragment{
         final int[] eatInts = new int[]{R.drawable.eat_boy, R.drawable.eat_girl};
         final int[] choosedInts = new int[]{R.drawable.drink_boy};
 
+        final int[] soundDrinkInts = new int[]{R.raw.drink_boy, R.raw.drink_girl};
+        final int[] soundEatInts = new int[]{R.raw.eat_boy, R.raw.eat_girl};
+        final int[] choosedSoundInts = new int[]{R.raw.drink_boy};
+
         drinkImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 choosedInts[0] = drinkInts[genderAnInt];
-                showChoose(choosedInts[0]);
+                choosedSoundInts[0] = soundDrinkInts[genderAnInt];
+                showChoose(choosedInts[0], choosedSoundInts[0]);
             }
         });
 
@@ -54,17 +60,28 @@ public class FoodWaterFragment extends Fragment{
             @Override
             public void onClick(View view) {
                 choosedInts[0] = eatInts[genderAnInt];
-                showChoose(choosedInts[0]);
+                choosedSoundInts[0] = soundEatInts[genderAnInt];
+                showChoose(choosedInts[0], choosedSoundInts[0]);
             }
         });
 
     }   // chooseImage
 
-    private void showChoose(int choosedInt) {
+    private void showChoose(int choosedInt, int soundChoosedInt) {
 
         ImageView imageView = getView().findViewById(R.id.imvChoosed);
         imageView.setImageResource(choosedInt);
         imageView.setVisibility(View.VISIBLE);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(getActivity(), soundChoosedInt);
+        mediaPlayer.start();
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release();
+            }
+        });
 
     }
 
